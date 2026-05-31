aa0d71c672086c1d06ae768f472862ed6263a758
unknown <mourad.meknioui@outlook.com>
2026-05-31 21:06:02 +0200

    add

--- Backend_fastApi/database/database.py
+++ Backend_fastApi/database/database.py
@@ -1,12 +1,18 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
-SQLALCHEMY_DATABASE_URI = "sqlite:///blog_api.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URI,connect_args={"check_same_thread":False})
+engine = create_engine(DATABASE_URL)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
 Base = declarative_base()
 
 def get_db():
